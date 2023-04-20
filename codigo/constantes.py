70e9837c4017b78eabf3191a5ca052ad3656c532
leosfreitas <leonardosf@al.insper.edu.br>
2023-04-20 10:27:44 -0300

    correção diretorios

--- codigo/constantes.py
+++ codigo/constantes.py
@@ -2,7 +2,7 @@ import pygame
 pygame.init()
 
 # Fontes 
-font_arc ='assets/fonts\JosefinSans-BoldItalic.ttf'
+font_arc ='../assets/fonts\JosefinSans-BoldItalic.ttf'
 font_titulo = pygame.font.Font(font_arc, 40)
 font_texto = pygame.font.Font(font_arc, 30)
 font_texto_popup = pygame.font.Font(font_arc, 35)
@@ -11,19 +11,19 @@ FONTE_TEXTO = font_texto
 FONTE_TEXTO_POPUP = font_texto_popup
 
 # Backgrounds
-FUNDO = pygame.image.load('assets/backgrounds\StartScreen/1.png')
-LUA = pygame.image.load('assets/backgrounds\StartScreen/2.png')
-NUVEM_1 = pygame.image.load('assets/backgrounds\StartScreen/3.png')
+FUNDO = pygame.image.load('../assets/backgrounds\StartScreen/1.png')
+LUA = pygame.image.load('../assets/backgrounds\StartScreen/2.png')
+NUVEM_1 = pygame.image.load('../assets/backgrounds\StartScreen/3.png')
 NUVEM_1_VEL = 0.5
-NUVEM_4 = pygame.image.load('assets/backgrounds\StartScreen/4.png')
+NUVEM_4 = pygame.image.load('../assets/backgrounds\StartScreen/4.png')
 NUVEM_4_VEL = - 0.5
-CHAO = pygame.image.load('assets/backgrounds\TelaJogo\CHAO TESTE.png')
-FUNDO_INICIO = pygame.image.load('assets/backgrounds\TelaJogo\Background DE FATO.png')
+CHAO = pygame.image.load('../assets/backgrounds\TelaJogo\CHAO TESTE.png')
+FUNDO_INICIO = pygame.image.load('../assets/backgrounds\TelaJogo\Background DE FATO.png')
 
 #
 # Sprites
-PLAYER_PARADO = pygame.image.load('assets\player\parado\Idle.png')
-PLAYER_ANDANDO = pygame.image.load('assets\player/andando\Walk.png')
+PLAYER_PARADO = pygame.image.load('../assets\player\parado\Idle.png')
+PLAYER_ANDANDO = pygame.image.load('../assets\player/andando\Walk.png')
 # Cores
 PRETO = (0, 0, 0)
 BRANCO = (255, 255, 255)
